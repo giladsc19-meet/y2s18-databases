9dc3dd60cce6614cb50c173c9ee7610f1fc2f05b
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 12:14:32 +0300

    dat

--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -1,11 +1,19 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Double
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Knowledge(Base):
+class Player(Base):
+	primary_key = column(Integer,primary_key)
+	name = column(String)
+	number = column(Integer)
+	height = column(Double)
+
+	def __repr__(Player):
+		
+
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
