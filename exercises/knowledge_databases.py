f5f68af66a114ff2b382ea49c613dba7cfacb42f
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 12:18:47 +0300

    dat

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -1,5 +1,4 @@
-from knowledge_model import Base, Knowledge
-
+from knowledge_model import Base, Player
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -8,20 +7,48 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_player(name, number, overating):
+	P = Player(name, number, overating)
+	session.add(P)
+	session.commit()
 
-def query_all_articles():
-	pass
+def query_all_players():
+	return session.query(Player).all() 
 
-def query_article_by_topic():
-	pass
+def query_player_by_topic(name):
+	return session.query(Player).filter_by(name=name).first()
 
-def delete_article_by_topic():
-	pass
+def delete_player_by_topic(name):
+	session.query(Player).filter_by(name=name).delete().first()
+	session.commit()
 
-def delete_all_articles():
-	pass
+def delete_all_players():
+	session.query(Player).delete()
+	session.commit()
 
-def edit_article_rating():
-	pass
+def edit_player_overating(number,newrate):
+	session.query(Player).filter_by(number=number).first().overating = newrate
+	session.commit()
+
+
+#print(query_player_by_topic("Michel Jorden"))
+#delete_player_by_topic("Jimmy Butler")
+edit_player_overating(21,90)
+print(query_all_players())
+# delete_all_players()
+
+# add_player("Michel Jorden", 23, 99)
+# add_player("Scottie Pippen", 33, 97)
+# add_player("Derrick Rose", 1, 94)
+# add_player("Artis Gilmore", 53, 94)
+# add_player("Dennis Rodman", 91, 93)
+# add_player("Jimmy Butler", 21, 90)
+# add_player("Jerry Sloan", 4 , 89)
+# add_player("Bob Love", 10, 88)
+# add_player("Joakim Noah", 13, 87)
+# add_player("Luol Deng", 9, 86)
+# add_player("Horace Grant", 54, 85)
+# add_player("B.J. Armstrong", 11, 85)
+# add_player("Toni Kukoc", 7, 85)
+# add_player("Charles Oakley", 34, 84)
+# add_player("Steve Kerr", 25, 81)
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -12,6 +12,11 @@ class Player(Base):
 	number = Column(Integer)
 	overating = Column(Float)
 
+	def __init__(self,name,number,overating):
+		self.name=name
+		self.number=number
+		self.overating=overating
+		
 	def __repr__(self):
 		if self.overating>=90:
 			return("An ALL-STAR Player! :)"
@@ -28,15 +33,16 @@ class Player(Base):
 			"name: {}\n"
 			"number: {}\n"
 			"overating: {}\n").format(self.name,self.number,self.overating)
-Jordan = Player(name = "Michael Jordan",number = 23,overating = 99.0)
-print(Jordan)
-Pippen = Player(name = "Scottie Pippen",number =33 ,overating = 97.0)
-print(Pippen)
-Rose = Player(name = "Derrick Rose",number = 1,overating = 94.0)
-print(Rose)
-Gilmore = Player(name = "Artis Gilmore",number = 53,overating = 94.0)
-print(Gilmore)
-Rodman = Player(name = "Dennis Rodman",number = 91,overating = 93.0)
-print(Rodman)
-Butler = Player(name = "Jimmy Butler",number = 21,overating = 90.0)
-print(Butler)
+
+# jordan = Player(name = "Michael Jordan",number = 23,overating = 99.0)
+# print(Jordan)
+# pippen = Player(name = "Scottie Pippen",number =33 ,overating = 97.0)
+# print(Pippen)
+# rose = Player(name = "Derrick Rose",number = 1,overating = 94.0)
+# print(Rose)
+# gilmore = Player(name = "Artis Gilmore",number = 53,overating = 94.0)
+# print(Gilmore)
+# rodman = Player(name = "Dennis Rodman",number = 91,overating = 93.0)
+# print(Rodman)
+# butler = Player(name = "Jimmy Butler",number = 21,overating = 90.0)
+# print(Butler)
